2a0d8ac610f45ea7dfe0b3d2a48ef64f28a9f75e
Brian Samson <brian@briansamson.com>
2023-03-30 11:12:00 -0500

    update slack initializer

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -2,3 +2,7 @@ Slack::Web::Client.configure do |config|
   config.token = ENV['SLACK_BOT_TOKEN']
   config.user_agent = "Gatewood/1.0 Slack Ruby Client/2.1.0"
 end
+
+Slack::Events.configure do |config|
+  config.signing_secret = ENV["SLACK_SIGNING_SECRET"]
+end
